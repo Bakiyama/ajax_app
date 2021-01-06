81b3cd16c107dd133679d25952dc9fc22f524f38
Bakiyama <htt.ikumi@gmail.com>
2021-01-06 14:34:01 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,13 +10,16 @@ class PostsController < ApplicationController
 
   def checked
     post = Post.find(params[:id])
-    if post.checked 
+    if post.checked # postsテーブルのレコードで、「checkedカラムの中身が入っていないか」を見る
       post.update(checked: false)
+      # false＝入っていたので、中身を除く
     else
       post.update(checked: true)
+      # true＝入っていないので、中身を加える
     end
 
     item = Post.find(params[:id])
-    render json: { post: item }
+    render json: { post: item } 
+    # itemにif文の判定後の内容を代入し、javascriptに渡す為にjsonに入れる。
   end
 end
